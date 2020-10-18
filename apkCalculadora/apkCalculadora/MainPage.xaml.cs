c0d1df29c8bcb6ed53a5b4bbac2a13c05a62610e
koryroot <73083604+koryroot@users.noreply.github.com>
2020-10-18 19:54:22 -0400

    primer commit

---
+++ apkCalculadora/apkCalculadora/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace apkCalculadora
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            wkOperacion.Items.Add("suma");
+            wkOperacion.Items.Add("resta");
+            wkOperacion.Items.Add("multiplicacion");
+            wkOperacion.Items.Add("division");
+            wkOperacion.SelectedIndexChanged += WkOperacion_SelectedIndexChanged;
+        }
+
+        private void WkOperacion_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String op = wkOperacion.SelectedItem.ToString();
+            int n1 = Int32.Parse(txtn1.Text);
+            int n2 = Int32.Parse(txtn2.Text);
+            double res = 0;
+
+            if (op == "suma")
+            {
+                res = n1 + n2;
+            }
+            else if (op == "resta")
+            {
+                res = n1 - n2;
+            }
+            else if (op == "multiplicacion")
+            {
+                res = n1 * n2;
+            }
+            else if (op == "division")
+            {
+                res = n1 / n2;
+            }
+            else
+            {
+                Console.WriteLine("no se pueden hacer calculos multiples");
+            }
+            lblRes.Text = res + "";
+        }
+    }
+}
